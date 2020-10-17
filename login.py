7ed699e3a2d688b6d9a451812e13c5e13ce80184
sh0tzz <56997476+sh0tzz@users.noreply.github.com>
2020-10-17 11:30:41 +0200

    Add files via upload

--- login.py
+++ login.py
@@ -1,5 +1,4 @@
 import pyautogui
-import window_manager
 
 class AutoLogin:
     def __init__(self, password_field_image, username, password):
@@ -7,29 +6,22 @@ class AutoLogin:
         self.username = username
         self.password = password
 
-    def login(self, stay_logged_in = False):
-        window_manager.bring_to_top('riot client')
+    def login(self):
         self.find_password_box(self.password_field_image)
         self.parse_values(self.password_box)
-        self.current_pos = pyautogui.position()
         self.fill_username(self.username_pos[0], self.username_pos[1])
         self.fill_password(self.password_pos[0], self.password_pos[1])
-        if stay_logged_in:
-            self.check_box()
         self.confirm(self.confirm_pos[0], self.confirm_pos[1])
-        self.go_back(self.current_pos[0], self.current_pos[1])
 
     def find_password_box(self, image):
         self.password_box = None
-        for i in range(10):
+        while self.password_box == None:
             self.password_box = pyautogui.locateOnScreen(self.password_field_image, grayscale = True)
-            if self.password_box != None:
-                break
 
-    def parse_values(self, password_box):
+    def parse_values(self, password_box) :
         self.password_pos = (password_box[0] + 10, password_box[1] + 10)
         self.username_pos = (self.password_pos[0], self.password_pos[1] - 75)
-        self.confirm_pos = (self.password_pos[0] + 120, self.password_pos[1] + 200)
+        self.confirm_pos = (self.password_pos[0] + 120, self.password_pos[1] + 150)
 
     def fill_username(self, username_x, username_y):
         pyautogui.click(username_x, username_y)
@@ -39,13 +31,5 @@ class AutoLogin:
         pyautogui.click(password_x, password_y)
         pyautogui.write(self.password)
 
-    def check_box(self):
-        pyautogui.press('tab')
-        pyautogui.press('space')
-
     def confirm(self, confirm_x, confirm_y):
-        pyautogui.press('tab')
-        pyautogui.press('space')
-
-    def go_back(self, current_x, current_y):
-        pyautogui.moveTo(current_x, current_y)
+        pyautogui.click(confirm_x, confirm_y)
